a150d8a9d838e6228690822aa1500220320a7391
azm0v7h <alihak31328@gmail.com>
2025-09-18 14:23:53 +0300

    Update Routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { register, login, getProfile, updateProfile } from "../controllers/authController.js";
 import { protect, isAdmin } from "../middlewares/authMiddleware.js";
+import {upload} from "../middlewares/uploadMiddleware.js"
 
 
 const router = express.Router();
@@ -8,6 +9,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getProfile);
-router.put("/update", protect, updateProfile);
+router.put("/update", protect, upload.single("profileImage"), updateProfile);
 
 export default router;
